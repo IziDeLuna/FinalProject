76136274632bad40de9ea4950704185af127ee5e
caortiz <caortiz@pointandpay.com>
2017-04-08 15:31:15 -0400

    Added MySQL

--- Project4App/app/controllers/welcome_controller.rb
+++ Project4App/app/controllers/welcome_controller.rb
@@ -3,6 +3,14 @@ class WelcomeController < ApplicationController
   end
 
   def login
-    render plain: params[:login].inspect
+
+    @username = params[:login][:username]
+    @password = params[:login][:password]
+    render plain: @username + " " +  @password
+    #search for the existance of @username and @password
+
+
+    #find level of account permissions and redirect to the appropriate view
+
   end
 end
